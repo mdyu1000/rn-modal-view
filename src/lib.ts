285d59aee8f5e19c1c81f98ae4a03db550cae849
k7yu <k7yu@3drens.com>
2021-06-09 23:06:09 +0800

    Remove custom animation function

--- src/lib.ts
+++ src/lib.ts
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { BackHandler } from 'react-native'
 
+// eslint-disable-next-line import/prefer-default-export
 export const useAndroidBack = (callback: () => void) => {
   useEffect(() => {
     const unsubscribe = BackHandler.addEventListener('hardwareBackPress', () => {
@@ -9,34 +10,4 @@ export const useAndroidBack = (callback: () => void) => {
     })
     return () => unsubscribe.remove()
   }, [callback])
-}
-
-/**
- * Custom slide-in animation
- * reference /node_modules/react-native-animatable/definition/sliding-entrances.js
- */
-export function makeSlideInTranslation(translationType: string, fromValue: number) {
-  return {
-    from: {
-      [translationType]: fromValue,
-    },
-    to: {
-      [translationType]: 0,
-    },
-  }
-}
-
-/**
- * Custom slide-out animation
- * reference /node_modules/react-native-animatable/definition/sliding-exits.js
- */
-export function makeSlideOutTranslation(translationType: string, fromValue: number) {
-  return {
-    from: {
-      [translationType]: 0,
-    },
-    to: {
-      [translationType]: fromValue,
-    },
-  }
 }
